2117d1540334f38d4fbbd6260423b89635ac65ad
coder-aastha <aasthadahal312@gmail.com>
2025-07-03 17:53:10 +0545

    Bugs fixed

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -11,27 +11,29 @@ cloudinary.config({
 export const uploadMedia = async (file) => {
   try {
     const uploadResponse = await cloudinary.uploader.upload(file, {
-      resource_type: "auto",
-      format:"mp4"
+      resource_type: "auto", // Let Cloudinary detect the resource type (image or video)
     });
     return uploadResponse;
   } catch (error) {
     console.log(error);
+    throw error; // Throw error to be handled by the calling function
   }
 };
+
 export const deleteMediaFromCloudinary = async (publicId) => {
   try {
-    await cloudinary.uploader.destroy(publicId);
+    await cloudinary.uploader.destroy(publicId, { resource_type: "image" }); // Specify resource_type as image
   } catch (error) {
     console.log(error);
+    throw error;
   }
 };
 
 export const deleteVideoFromCloudinary = async (publicId) => {
-    try {
-        await cloudinary.uploader.destroy(publicId,{resource_type:"video"});
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
+  try {
+    await cloudinary.uploader.destroy(publicId, { resource_type: "video" }); // Specify resource_type as video
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
